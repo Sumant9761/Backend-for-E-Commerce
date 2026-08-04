7c505f137fa47971ccc3faadbcf232566fb7aaf2
Sumant9761 <sumantkumaryadav7037@gmail.com>
2026-08-04 11:58:06 +0530

    I have completed userRoute till userDetails

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -16,7 +16,6 @@ cloudinary.config({
   secure: true,
 });
 
-
 export async function registerUserController(req, res) {
   try {
     let user;
@@ -84,7 +83,6 @@ export async function registerUserController(req, res) {
   }
 }
 
-
 export async function verifyEmailController(req, res) {
   try {
     const { email, otp } = req.body;
@@ -101,10 +99,11 @@ export async function verifyEmailController(req, res) {
     const isCodevalid = user.otp === otp;
     const isNotExpired = user.otpExpires > Date.now();
 
-    if (!isCodevalid && !isNotExpired) {
+    if (isCodevalid && isNotExpired) {
       user.verify_email = true;
       user.otp = null;
       user.otpExpires = null;
+
       await user.save();
 
       return res.status(200).json({
@@ -142,7 +141,6 @@ export async function verifyEmailController(req, res) {
   }
 }
 
-
 export async function loginUserController(req, res) {
   try {
     const { email, password } = req.body;
@@ -159,6 +157,14 @@ export async function loginUserController(req, res) {
     if (user.status !== "Active") {
       return res.status(400).json({
         message: "Contact to admin",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.verify_email !== true) {
+      return res.status(400).json({
+        message: "Your email is not verify yet, please verify your email first",
         error: true,
         success: false,
       });
@@ -206,56 +212,53 @@ export async function loginUserController(req, res) {
   }
 }
 
-
 //logout user
 export async function logoutUserController(req, res) {
-    try{
-        const userid = req.userId;  // auth middleware
+  try {
+    const userid = req.userId; // auth middleware
 
-        const cookieOption = {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-        };
-        res.clearCookie("accessToken", cookieOption);
-        res.clearCookie("refreshToken", cookieOption);
+    const cookieOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    };
+    res.clearCookie("accessToken", cookieOption);
+    res.clearCookie("refreshToken", cookieOption);
 
-        const removeRefreshToken = await Usermodel.findByIdAndUpdate(userid, {
-            refresh_token: "",
-        });
+    const removeRefreshToken = await Usermodel.findByIdAndUpdate(userid, {
+      refresh_token: "",
+    });
 
-        return res.json({
-            message: "User logged out successfully",
-            error: false,
-            success: true,
-        });
-
-    }catch(error){
-        return res.status(500).json({
-            message: error.message || error,
-            error: true,
-            success: false,
-          });
-    }
+    return res.json({
+      message: "User logged out successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
 }
-
 
 // image upload
 var imagesArr = [];
-export async function userAvatarController(req, res){
-  try{
+export async function userAvatarController(req, res) {
+  try {
     imagesArr = [];
 
-    const userId = req.userId;   //auth middleware
+    const userId = req.userId; //auth middleware
     const image = req.files;
 
     const user = await Usermodel.findOne({ _id: userId });
-    if(!user){
+    if (!user) {
       return res.status(500).json({
         message: "User not found",
         error: true,
         success: false,
-      }); 
+      });
     }
 
     // First remove image from cloudinary
@@ -265,30 +268,29 @@ export async function userAvatarController(req, res){
 
     const imageName = avatar_image.split(".")[0];
 
-    if(imageName){
+    if (imageName) {
       const response = await cloudinary.uploader.destroy(
         imageName,
         (error, result) => {
           // console.log(error, res)
-        }
+        },
       );
     }
 
-      
     const options = {
-        user_filename: true,
-        unique_filename: false,
-        overwrite: false,
-      };
+      user_filename: true,
+      unique_filename: false,
+      overwrite: false,
+    };
 
-    for(let i = 0; i < image.length; i++){
+    for (let i = 0; i < image.length; i++) {
       const img = await cloudinary.uploader.upload(
         image[i].path,
         options,
         function (error, result) {
           imagesArr.push(result.secure_url);
           fs.unlinkSync(`uploads/${image[i].filename}`);
-        }
+        },
       );
     }
 
@@ -297,10 +299,195 @@ export async function userAvatarController(req, res){
 
     return res.status(200).json({
       _id: userId,
-      avatar: imagesArr[0]
+      avatar: imagesArr[0],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function removeImageFromCloudinary(req, res) {
+  const imgUrl = req.query.img;
+  const urlArr = imgUrl.split("/");
+  const image = urlArr[urlArr.length - 1];
+
+  const imageName = image.split(".")[0];
+
+  if (imageName) {
+    const response = await cloudinary.uploader.destroy(
+      imageName,
+      (error, result) => {
+        // console.log(error, res)
+      },
+    );
+
+    if (response) {
+      res.status(200).send(response);
+    }
+  }
+}
+
+// Update user details
+export async function updateUserDetails(req, res) {
+  try {
+    const userId = req.userId;
+    const { name, email, mobile, password } = req.body;
+
+    const userExist = await Usermodel.findById(userId);
+    if (!userExist) return res.status(400).send("The user cannot be updated!");
+
+    let verifyCode = "";
+
+    if (email !== userExist.email) {
+      verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    }
+
+    let hashPassword = "";
+
+    if (password) {
+      const salt = await bcryptjs.genSalt(10);
+      hashPassword = await bcryptjs.hash(password, salt);
+    } else {
+      hashPassword = userExist.password;
+    }
+
+    const updateUser = await Usermodel.findByIdAndUpdate(
+      userId,
+      {
+        name,
+        mobile,
+        email,
+        verify_email: email !== userExist.email ? false : true,
+        password: hashPassword,
+        otp: verifyCode !== "" ? verifyCode : null,
+        otpExpires: verifyCode !== "" ? Date.now() + 600000 : "",
+      },
+      { new: true },
+    );
+
+    if (email !== userExist.email) {
+      // Send verification email
+      await sendEmailFun({
+        to: email,
+        subject: "Verify email from Ecommerce App",
+        text: "",
+        html: VerificationEmail(name, verifyCode),
+      });
+    }
+
+    return res.json({
+      message: "User updated successfully",
+      error: false,
+      success: true,
+      user: updateUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+// Forgot Password
+export async function forgotPasswordController(req, res) {
+  try {
+    const { email } = req.body;
+
+    const user = await Usermodel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Email not available",
+        error: true,
+        success: false,
+      });
+    }
+
+    let verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+
+    user.otp = verifyCode;
+    user.otpExpires = Date.now() + 600000;
+
+    await user.save();
+
+    await sendEmailFun({
+      to: email,
+      subject: "Verify email from Ecommerce App",
+      text: "",
+      html: VerificationEmail(user?.name, verifyCode),
     });
 
-  }catch(error){
+    return res.json({
+      message: "Check your email",
+      error: false,
+      success: true,
+    });
+  } catch {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+export async function verifyForgotPasswordOtp(req, res) {
+  try {
+    const { email, otp } = req.body;
+
+    const user = await Usermodel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Email not available",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (!email || !otp) {
+      return res.status(400).json({
+        message: "Provide required field email, otp.",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (otp !== user.otp) {
+      return res.status(400).json({
+        message: "Invalid OTP",
+        error: true,
+        success: false,
+      });
+    }
+
+    const currentTime = new Date().toISOString();
+
+    if (user.otpExpires < currentTime) {
+      return res.status(400).json({
+        message: "OTP is expired",
+        error: true,
+        success: false,
+      });
+    }
+
+    user.otp = "";
+    user.otpExpires = "";
+    user.verify_email = true;
+
+    await user.save();
+
+    return res.json({
+      message: "Verify OTP successfully",
+      error: false,
+      success: true,
+    });
+
+  } catch (error) {
     return res.status(500).json({
       message: error.message || error,
       error: true,
@@ -310,25 +497,142 @@ export async function userAvatarController(req, res){
 }
 
 
+//Reset Password
+export async function resetPassword(req, res) {
+  try {
+    const { email, newPassword, confirmPassword } = req.body;
 
-export async function removeImageFromCloudinary(req, res){
-  const imgUrl = req.query.img;
-  const urlArr = imgUrl.split("/");
-  const image = urlArr[urlArr.length - 1];
-
-  const imageName = image.split(".")[0];
-
-  if(imageName){
-    const response = await cloudinary.uploader.destroy(
-      imageName,
-      (error, result) => {
-        // console.log(error, res)
-      }
-    );
-
-    if(response){
-      res.status(200).send(response);
+    // Validate inputs
+    if (!email) {
+      return res.status(400).json({
+        message: "Email not available",
+        error: true,
+        success: false,
+      });
     }
+
+    if (!newPassword || !confirmPassword) {
+      return res.status(400).json({
+        error: true,
+        success: false,
+        message: "Please provide new password and confirm password",
+      });
+    }
+
+    // Check if passwords match
+    if (newPassword !== confirmPassword) {
+      return res.status(400).json({
+        message: "New password and confirm password must be the same",
+        error: true,
+        success: false,
+      });
+    }
+
+    // Find user by email
+    const user = await Usermodel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Email not available",
+        error: true,
+        success: false,
+      });
+    }
+
+    // Hash the new password
+    const salt = await bcryptjs.genSalt(10);
+    const hashPassword = await bcryptjs.hash(newPassword, salt);
+
+    // Update user password
+    user.password = hashPassword;
+    user.otp = null;
+    user.otpExpires = null;
+
+    await user.save();
+
+    return res.json({
+      message: "Password updated successfully.",
+      error: false,
+      success: true,
+    });
+    
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
   }
-  
+}
+
+
+export async function refreshToken(req, res) {
+  try {
+    const refreshToken =
+      req.cookies.refreshToken ||
+      req?.headers?.authorization?.split(" ")[1]; // [bearer token]
+    if (!refreshToken) {
+      return res.status(401).json({
+        message: "Invalid Token",
+        error: true,
+        success: false,
+      });
+    }
+
+    const verifyToken = await jwt.verify(refreshToken, process.env.SECRET_KEY_REFRESH_TOKEN);
+    if (!verifyToken) {
+      return res.status(401).json({
+        message: "Token is expired",
+        error: true,
+        success: false,
+      });
+    }
+
+    const userId = verifyToken?._id;
+    const newAccessToken = await generatedAccessToken(userId);
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+    res.cookie("accessToken", newAccessToken, cookiesOption);
+
+    return res.json({
+      message: "New Access token generated",
+      error: false,
+      success: true,
+      data: {
+        accessToken: newAccessToken,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+
+//Get login user details
+export async function userDetails(req, res) {
+  try {
+    const userId = req.userId;
+
+    const user = await Usermodel.findById(userId).select("-password -refresh_token");
+    //Here I don't need password and refresh_token so i minus of both
+    return res.json({
+      message: "User details",
+      data: user,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something is wrong",
+      error: true,
+      success: false,
+    });
+  }
 }
--- route/user.route.js
+++ route/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { registerUserController, verifyEmailController, loginUserController, logoutUserController, 
-userAvatarController, removeImageFromCloudinary } from '../controllers/user.controller.js';
+userAvatarController,  removeImageFromCloudinary, updateUserDetails, forgotPasswordController,
+verifyForgotPasswordOtp, resetPassword, refreshToken, userDetails} from '../controllers/user.controller.js';
 import auth from '../middlewares/auth.js';
 import upload from '../middlewares/multer.js';
 
@@ -13,6 +14,11 @@ userRouter.post('/login', loginUserController);
 userRouter.get('/logout', auth, logoutUserController);
 userRouter.put('/user-avatar', auth, upload.array('avatar'), userAvatarController);
 userRouter.delete('/deleteImage', auth, removeImageFromCloudinary);
-
+userRouter.put('/:id', auth, updateUserDetails);
+userRouter.post('/forgot-password', forgotPasswordController);
+userRouter.post('/verify-forgot-password-otp', verifyForgotPasswordOtp);
+userRouter.post('/reset-password', resetPassword);
+userRouter.post('/refresh-Token', refreshToken);
+userRouter.get('/user-details',auth, userDetails);
 
 export default userRouter;
